20e2c37762ccb0133bcce155bb3c6ad7587c255d
Abhi <kartarsinghabhi2007@gmail.com>
2026-09-11 20:54:13 +0530

    Changed model to local modal

--- Backend/AI/config.py
+++ Backend/AI/config.py
@@ -1,9 +1,12 @@
 from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
 from utils.settings import settings
 from dotenv import load_dotenv
 
 load_dotenv()
+
+Base_URL = "http://localhost:11434"
 
 FEATHERLESS_API_KEY= settings.FEATHERLESS_API_KEY
 GROQ_API_KEY= settings.GROQ_API_KEY
@@ -14,17 +17,29 @@ if not FEATHERLESS_API_KEY:
 if not GROQ_API_KEY:
     raise RuntimeError("Missing GROQ_API_KEY in the environment")
 
-vlm = ChatOpenAI(
-    api_key=FEATHERLESS_API_KEY,
-    model="Qwen/Qwen3-VL-4B-Instruct",
-    base_url="https://api.featherless.ai/v1",
+# vlm = ChatOpenAI(
+#     api_key=FEATHERLESS_API_KEY,
+#     model="Qwen/Qwen3-VL-4B-Instruct",
+#     base_url="https://api.featherless.ai/v1",
+# )
+
+vlm = ChatOllama(
+    base_url=Base_URL,
+    model='qwen3-vl:8b',
+    temperature=0.7
 )
 
-llm = ChatGroq(
-    model="openai/gpt-oss-120b",
-    temperature=0.3,
-    disable_streaming=True,
-    reasoning_effort="low"
+# llm = ChatGroq(
+#     model="openai/gpt-oss-120b",
+#     temperature=0.3,
+#     disable_streaming=True,
+#     reasoning_effort="low"
+# )
+
+llm = ChatOllama(
+    base_url=Base_URL,
+    model='llama3.1:8b',
+    temperature=0.7,
 )
 
 REMBG_MODEL_NAME = "u2net"
